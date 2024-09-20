fdbfeed00d3f257d3309b78d61a9812143e847c2
Deepanshu Sahu <83969256+dee077@users.noreply.github.com>
2024-09-20 19:51:49 +0530

    final commit

--- server.js
+++ server.js
@@ -12,17 +12,32 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: '*',
-  credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
 app.use(express.json());
 
-try {
-  mongoose.connect(process.env.MONGODB_URI);
-  console.log("Connected to MongoDB");
-} catch (error) {
-  console.log("MongoDB Connection failed")
+app.options('*', cors());
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("MongoDB Connection failed", error);
+    process.exit(1); // Exit the application if the connection fails
+  }
 }
+
+connectDB();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello NetflixGPT Backend');
+});
 
 app.use('/api', testRoute,);
 app.use('/api/auth', authRoutes);
@@ -31,3 +46,5 @@ app.use('/api/auth', authRoutes);
 app.listen(PORT, () => {
   console.log(`Listening at port: ${PORT}`);
 });
+
+module.exports = app;
